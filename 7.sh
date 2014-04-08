020466968323b58b138e049539b96be0f0e1bfa4
amas <zhoujb.cn@gmail.com>
2014-04-08 19:35:34 +0800

    change 7.sh

--- 7.sh
+++ 7.sh
@@ -3,16 +3,13 @@ source='main() { puts("hello world"); }'
 
 function cc_size() {
     local tag=$1
-    local src
     local out=$1.out
     
     function size() {
         wc -c $out
     }
 
-    src=$(<&0)
-    <<< $src | gcc -xc -o $out - && size
-    rm $out
+    <<< $(<&0) | gcc -xc -o $out - && size && rm $out
 }
 
 <<< $source | cc_size "1"
