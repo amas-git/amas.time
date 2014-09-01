7a006dfe67a7c067bf8e22d34316fe6964fb3ea7
amas <zhoujb.cn@gmail.com>
2014-09-01 20:05:33 +0800

    add c

---
+++ fork.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(char ** argv) {
+    int pid = fork();
+    printf("fork()=%d\n", pid);
+    if(pid) {
+        printf("1> PID : %5d PARENT : %5d\n", getpid(), getppid());
+    } else {
+        printf("2> PID : %5d PARENT : %5d\n", getpid(), getppid());
+    }
+    return 0;
+}
---
+++ true-or-false.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(char ** argv) {
+    if(0) {
+        printf("true");
+    } else {
+        printf("false");
+    }
+    return 0;
+}
