e873eee9ff4655ed185e8f1c2164ac1dced27c73
amas <zhoujb.cn@gmail.com>
2018-07-03 22:04:28 +0800

    完成的初步计算

--- nodejs/redstar/src/blacksheep.js
+++ nodejs/redstar/src/blacksheep.js
@@ -11,7 +11,7 @@ function calcPrice(txs) {
     let { value, ammount } = txs.reduce((a, b) => {
         return {value : a.value + (b.price * b.ammount), ammount:(a.ammount + b.ammount) }
     }, {value: 0.0, ammount:0.0});
-    return { price: (value / ammount), ammount: ammount, value: value.toFixed(2)};
+    return { price: (value / ammount), ammount: ammount, value: value};
 }
 
 function calcTargetPrice(txs,step=0.05,max=20) {
@@ -20,7 +20,7 @@ function calcTargetPrice(txs,step=0.05,max=20) {
     for(let i=1; i<max; ++i) {
         let grow = (1+step*i);
         let target =  grow * value;
-        rs.push({grow: grow.toFixed(2), prof: (target - value).toFixed(2), price: (target/ammount)});
+        rs.push({grow: grow, prof: (target - value), price: (target/ammount)});
     }
     return rs;
 }
@@ -36,11 +36,142 @@ function valueOf(base=100, inc=0.05, times=10) {
 const txs = [
     {price: 520.8, ammount: 0.477},
     {price: 445.0, ammount: 0.2549},
+
     //{price: 300.0, ammount: 0.3},
 ];
 
-console.log(calcPrice(txs));
-console.log(calcTargetPrice(txs));
+// console.log(calcPrice(txs));
+// console.log(calcTargetPrice(txs));
 
 //valueOf(100, 0.05, 50);
 //console.log(100.0*((1+0.05)^10));
+
+class Barn {
+    constructor() {
+        this.value = 0;
+        this.txs_b = [];
+        this.txs_s = [];
+        this.ammount = 0;
+        this.price0 = 0;
+    }
+
+    buy(price, ammount) {
+        this.txs_b.push({price: price, ammount: ammount});
+        this.ammount += ammount;
+    }
+
+    sell(price, ammount) {
+        if(this.ammount < ammount) {
+            throw Error("SOLD OUT!!!");
+        }
+
+        this.txs_s.push({price: price, ammount: ammount});
+        this.ammount -= ammount;
+    }
+
+    print() {
+        // 投入多少钱
+        let {price:price0, ammount:ammount0, value:value_in } = calcPrice(this.txs_b);
+        console.log(`BUY : ${price0.toFixed(2)} ${ammount0} ${value_in.toFixed(2)}`);
+
+
+
+        let {price:price1, ammount:ammount1, value:value_out} = calcPrice(this.txs_s);
+        console.log(`SELL: ${price1.toFixed(2)} ${ammount1} ${value_out.toFixed(2)}`);
+        // 收回多少钱
+
+        let left = price0 * this.ammount;
+        let prof = left + value_out - value_in;
+
+        console.log(`PROF: ${prof.toFixed(2)} LEFT: ${this.ammount}`);
+        //
+        // console.log(this.txs_s);
+        // console.log(this.txs_b)
+    }
+}
+
+
+// {price: 520.8, ammount: 0.477},
+// {price: 445.0, ammount: 0.2549},
+let eth_usdt = new Barn();     // 0.7319 个  361.85
+eth_usdt.buy(520.8, 0.477);    // 248.4216
+eth_usdt.buy(445.0, 0.2549);   // 113.4216
+eth_usdt.sell(530.0, 0.2549);
+eth_usdt.sell(530.0, 0.477);
+eth_usdt.print();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Fcoin {
+    constructor() {
+        this.AMMOUNT = 100;
+        this.mine = 51;
+        this.locked = 49;
+        this.mined = 0;
+        this.usable = 0;
+        this.unlock = 0;
+    }
+
+    calc_unlock(mined) {
+        let unlock = this.locked * (mined / this.mine);
+
+        this.locked  -= unlock; // 锁定
+        this.mined   += mined;  // 挖矿部分
+        this.mine    -= mined;
+        this.unlock  += unlock; // 已经解锁部分
+        this.usable = this.mined + this.unlock;
+        let r  = {unlock: this.unlock.toFixed(2), locked: this.locked.toFixed(2), mined: this.mined.toFixed(2), LT: (this.mined/0.51).toFixed(2), usable: this.usable.toFixed(2)};
+
+        return JSON.stringify(r, null, 4);
+    }
+
+
+    doMine(mineable) {
+        this.mine  -= mineable;
+        this.mined += mineable;
+    }
+
+    // 收入分配
+    calcReword(fcoin_ammount, fee) {
+        let reword = (fcoin_ammount/this.usable) * fee * 0.8;
+        return reword;
+    }
+
+    calcFTmineable(fee, ft_price) {
+        let mineable = fee/ft_price;
+        return mineable;
+    }
+
+}
+//
+// fcoin = new Fcoin();
+//
+// // 第一天: 总手续费1, FT价格0.1
+// console.log(fcoin.calcFTmineable(1, 0.1)); // 10FT
+// // fcoin.doMine(10);
+//
+// // 第二天:
+// // fcoin.doMine(10);
+// console.log(fcoin.calc_unlock(10));
+// console.log(fcoin.calc_unlock(20));
+//
+//
+// // 第三天:
+// //fcoin.doMine(10);
+// // console.log(fcoin.calc_unlock(10));
+// // fcoin.doMine(10);
+// // console.log(fcoin.calc_unlock(10));
+// // console.log(fcoin.calc_unlock(1));
+// // console.log(fcoin.calc_unlock(1));
+// // console.log(fcoin.calc_unlock(1));
