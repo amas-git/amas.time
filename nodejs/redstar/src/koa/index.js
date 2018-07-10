213975a162e2d28cf2b5012049bfd5c3ce64b084
amas <zhoujb.cn@gmail.com>
2018-07-10 22:09:48 +0800

    added vue project

--- nodejs/redstar/src/koa/index.js
+++ nodejs/redstar/src/koa/index.js
@@ -1,22 +1,25 @@
 const Koa = require("koa");
 const Router = require('koa-router');
+const BodyParser = require('koa-bodyparser');
 
 const app = new Koa();
 const router = new Router();
 
-router.get("/users/:id", (ctx, next) => {
+router.all("/users/:id", (ctx, next) => {
         const r = ctx.request.origin;
         ctx.state.userId = `UID:${ctx.params.id}`;
         next();
     },
     (ctx) => {
-        ctx.body = `${ctx.state.userId}`;
+        //ctx.body = `${ctx.state.userId}`;
+        ctx.body = JSON.stringify(ctx.request.body) + JSON.stringify(ctx.request.query);
     }
     );
 
 // app.log = console.log;
 
 app.use(require('./middleware/response_time'));
+app.use(new BodyParser());
 // app.use(async (ctx) => {
 //     const r = ctx.request.origin;
 //     ctx.body = `${r}\n`+JSON.stringify(ctx,null,4);
@@ -24,4 +27,4 @@ app.use(require('./middleware/response_time'));
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-app.listen(3000)
+app.listen(3000);
